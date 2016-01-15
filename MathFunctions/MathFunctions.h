55242ae2ccc445597226157d88b4b2581dcb18f4
Binlong Li <herbert19lee@gmail.com>
2016-01-14 21:45:52 -0800

    finish step 2

---
+++ MathFunctions/MathFunctions.h
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <math.h>
+
+double mysqrt(double in);
---
+++ MathFunctions/mysqrt.cxx
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <math.h>
+#include "MathFunctions.h"
+
+double mysqrt(double in){
+	return sqrt(in);
+}
